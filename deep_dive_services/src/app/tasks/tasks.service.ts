530c10649dab94aefab73f91307847d03a55176d
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2024-07-18 18:14:27 +0200

    finished deep_dive_services topic

---
+++ deep_dive_services/src/app/tasks/tasks.service.ts
@@ -0,0 +1,31 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
+
+  allTasks = this.tasks.asReadonly();
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((prevVal) => [...prevVal, newTask]);
+    this.loggingService.log('ADDED TASK with title ' + taskData.title);
+  }
+  updateTasksStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((prevVal) =>
+      prevVal.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+    this.loggingService.log('CHANGE TASK TO ' + newStatus);
+  }
+}

d79f87bda33d6d0bf99773b0876943b92e84cf8b
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2024-07-20 11:08:44 +0200

    finished HTTP requests topic

---
+++ http_requests/src/main.ts
@@ -0,0 +1,38 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import {
+  HttpEventType,
+  HttpHandler,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
+
+function loggingInterseptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  const req = request.clone({
+    headers: request.headers.set('X-DEBUG', 'TESTING'),
+  });
+  console.log('[Outgoing Request');
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Incoming Response');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(withInterceptors([loggingInterseptor]))],
+}).catch((err) => console.error(err));
